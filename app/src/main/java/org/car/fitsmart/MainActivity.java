d2a4f58a81d1dc0eb0ebcc591fff3285e51427a6
carlaAlvReg <carlaalvrego@gmail.com>
2023-11-09 12:02:07 +0100

    Initial commit

---
+++ app/src/main/java/org/car/fitsmart/MainActivity.java
@@ -0,0 +1,32 @@
+package org.car.fitsmart;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu( menu );
+        this.getMenuInflater().inflate( R.menu.menu_e1, menu );
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        boolean toret = false;
+        switch( menuItem.getItemId() ) {
+            
+
+        }
+        return toret;
+    }
+}
