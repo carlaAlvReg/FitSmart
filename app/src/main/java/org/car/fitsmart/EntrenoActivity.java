0101c47027b2f738a15b2d037a319ae96a1faed5
carlaAlvReg <carlaalvrego@gmail.com>
2023-12-14 18:19:20 +0100

    Entrenos hechos

--- app/src/main/java/org/car/fitsmart/EntrenoActivity.java
+++ app/src/main/java/org/car/fitsmart/EntrenoActivity.java
@@ -1,6 +1,8 @@
 package org.car.fitsmart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,41 +10,65 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import org.car.fitsmart.adaptadores.ListaEjerciciosAdapter;
+import org.car.fitsmart.adaptadores.SpinnerDiaAdapter;
+import org.car.fitsmart.db.DbEjercicios;
+import org.car.fitsmart.db.DbHelper;
+import org.car.fitsmart.db.Ejercicio;
+
+import java.util.ArrayList;
+
 public class EntrenoActivity extends AppCompatActivity {
+
+    RecyclerView lvListaNE;
+    DbHelper dbHelper = new DbHelper(this);
+    ArrayList<Ejercicio> listaArrayEjercicios;
+
+    DbEjercicios dbEjercicios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entreno);
+        lvListaNE = findViewById(R.id.lvListaNE);
 
         Spinner spinner = findViewById(R.id.SpinnerDia);
 
         // Obtén la lista de días desde los recursos
         String[] daysArray = getResources().getStringArray(R.array.days_array);
 
-        // Crea un adaptador para el Spinner
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, daysArray);
-
-        // Especifica el diseño del dropdown
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Crea un adaptador personalizado para el Spinner
+        SpinnerDiaAdapter adapter = new SpinnerDiaAdapter(this, daysArray);
 
         // Asigna el adaptador al Spinner
         spinner.setAdapter(adapter);
 
+        dbEjercicios = new DbEjercicios(EntrenoActivity.this);
         // Maneja la selección de elementos
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                // Maneja la selección del día
+                // Cuando se selecciona un elemento en el Spinner, se ejecuta este código.
                 String selectedDay = daysArray[position];
-                // Puedes realizar acciones con el día seleccionado aquí
+                cargarEjerciciosPorDia(selectedDay);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parentView) {
-                // Maneja la situación cuando no se ha seleccionado nada
+                // Puedes manejar la situación cuando no se ha seleccionado nada, si es necesario.
             }
         });
+
+        // Configura el RecyclerView inicialmente con el día seleccionado por defecto
+        lvListaNE.setLayoutManager(new LinearLayoutManager(this));
+        String selectedDay = daysArray[spinner.getSelectedItemPosition()];
+        cargarEjerciciosPorDia(selectedDay);
+    }
+
+    private void cargarEjerciciosPorDia(String diaSeleccionado) {
+        listaArrayEjercicios = dbEjercicios.mostrarEjerciciosPorDia(diaSeleccionado);
+        ListaEjerciciosAdapter adapter = new ListaEjerciciosAdapter(listaArrayEjercicios);
+        lvListaNE.setAdapter(adapter);
     }
 
 }
